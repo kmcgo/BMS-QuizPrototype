24383d148c66594f959acefc2280a969ae7f845c
Keegan McGonigal <keegan@rover-220-181.rovernet.mtu.edu>
2020-02-16 14:38:28 -0500

    Initial commit

---
+++ app/src/main/java/com/example/bmsquizprototype/Category.java
@@ -0,0 +1,39 @@
+package com.example.bmsquizprototype;
+
+public class Category {
+
+    public static final int DRUG = 1;
+    public static final int SALES = 2;
+    public static final int DOCTORS = 3;
+
+    private int id;
+    private String name;
+
+    public Category() {
+    }
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return getName();
+    }
+}
